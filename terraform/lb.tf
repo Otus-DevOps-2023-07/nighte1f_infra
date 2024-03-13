39e5aa8030ffb5a822ec1d4d405e07c7732eb1f6
Kirill Butin <k.butin@dom.ru>
2024-03-13 10:58:56 +0300

    commented provider

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,6 +1,6 @@
 resource "yandex_lb_network_load_balancer" "reddit" {
   name = "lb-reddit"
-  deletion_protection = "false"
+  #deletion_protection = "false"
   listener {
     name = "reddit-listener"
     port = 9292
